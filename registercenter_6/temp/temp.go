19251edbf8087e2c90a42b2bc63aabe4b8d7e924
rookieyx <lyxrookie@qq.com>
2024-01-25 19:22:53 +0800

    misson6: registercenter

---
+++ registercenter_6/temp/temp.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	
+	
+)
+
+type Mystruct struct {} 
+
+func main() {
+	var b []int
+	c := b == nil
+	fmt.Println(c)
+}
---
+++ registercenter_6/xclient/xclient.go
@@ -0,0 +1,123 @@
+package xclient
+
+import (
+	"context"
+	"fmt"
+	. "go_geerpc/registercenter_6"
+	"io"
+	"reflect"
+	"strings"
+	"sync"
+)
+
+type XClient struct {
+	d Discovery
+	mode SelectMode
+	opt *Option
+	mu sync.Mutex
+	clients map[string]*Client
+}
+
+var _ io.Closer = (*XClient) (nil)
+
+func NewXClient(d Discovery, mode SelectMode, opt *Option) *XClient {
+	return &XClient{d: d, mode: mode, opt: opt, clients: make(map[string]*Client)}
+}
+
+func(xc *XClient) Close() error {
+	xc.mu.Lock()
+	defer xc.mu.Unlock()
+	for key, client := range xc.clients {
+		_ = client.Close()
+		delete(xc.clients, key)
+	}
+	return nil
+}
+
+func (xc *XClient) dial (rpcAddr string) (*Client, error) {
+	xc.mu.Lock()
+	defer xc.mu.Unlock()
+	client, ok := xc.clients[rpcAddr]
+	if ok && !client.IsAvailable() {
+		_ = client.Close()
+		delete(xc.clients, rpcAddr)
+		client = nil
+	}
+	if client == nil {
+		parts := strings.Split(rpcAddr, "@")
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("rpc client err: wrong format '%s', expect protocol@addr", rpcAddr)
+		} 
+		protocol, addr := parts[0], parts[1]
+		// bug: can't use := here
+		//client, err := Dial(protocol, addr, xc.opt)
+		var err error
+		client, err = Dial(protocol, addr, xc.opt)
+		if client == nil {
+			err =  fmt.Errorf("client is nil")
+			return nil, err
+		}
+		if err != nil {
+			return nil, err
+		}
+		xc.clients[rpcAddr] = client
+	}
+	return client, nil
+}
+
+func (xc *XClient) call(rpcAddr string, ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	client, err := xc.dial(rpcAddr)
+	
+	if err != nil {
+		return err
+	}
+	
+	return client.Call(ctx, serviceMethod, args, reply)
+}
+
+// Call invokes the named function, waits for it to complete,
+// and returns its error status.
+// xc will choose a proper server.
+func (xc *XClient) Call(ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	rpcAddr, err := xc.d.Get(xc.mode)
+	if err != nil {
+		return err
+	}
+	return xc.call(rpcAddr, ctx, serviceMethod, args, reply)
+}
+
+func (xc *XClient) Broadcast(ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	servers, err := xc.d.GetAll()
+	if err != nil {
+		return err
+	}
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	var e error
+	replyDone := reply == nil 
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	for _, rpcAddr := range servers {
+		wg.Add(1)
+		go func(rpcAddr string) {
+			defer wg.Done()
+			var clonedReply interface{}
+			if reply != nil {
+				clonedReply = reflect.New(reflect.ValueOf(reply).Elem().Type()).Interface()
+			}
+			err := xc.call(rpcAddr, ctx, serviceMethod, args, clonedReply)
+			mu.Lock()
+			if err != nil && e == nil {
+				e = err
+				cancel()
+			}
+			if err == nil && !replyDone {
+				reflect.ValueOf(reply).Elem().Set(reflect.ValueOf(clonedReply).Elem())
+				replyDone = true
+			}
+			mu.Unlock()
+		}(rpcAddr)
+	}
+	wg.Wait()
+	return e
+}
